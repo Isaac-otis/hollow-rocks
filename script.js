c8e29d5343091f7933c5f10bbb883a1c5bc7dec2
Glitch (glitch-blank-website) <none>
2021-12-10 05:10:20 +0000

    🐕🏓 Checkpoint ./script.js:35586298/651

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-document.querySelector("container");
-document.querySelector("gif");
+// We'll create two constants
+const sectionTag = document.querySelector("section");
+const button = document.querySelector("button");
+
+
+// we'll add an event listener to the button to check for clicks
+button.addEventListener("click", function() {
+  
+  // we'll toggle a class name and then use that particular class to add our styles
+  sectionTag.classList.toggle("new-class");
+});
