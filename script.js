ef193f75abd2d67b48794a75275052279f3ad209
Glitch (glitch-blank-website) <none>
2021-12-10 07:39:43 +0000

    🌾😎 Checkpoint ./script.js:35586298/493 ./style.css:35586298/496 ./index.html:35586298/6837

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ const sectionTag = document.querySelector("section");
 const button = document.querySelector("button");
 
 button.addEventListener("click", function() {
-sectionTag.classList.toggle("new-class");
-});
+
+  sectionTag.classList.toggle("new-class");
+
+})
+// we set up 3 constants
+// 1. the ticker wrapper (the area where the images are scrolling)
+// 2. the list of items
+// 3. the copy (clone of our list)
+const ticker = document.querySelector('.ticker')
+const list = document.querySelector('.ticker__list')
+const clone = list.cloneNode(true)
+
+// then we keep appending our list inside the ticker
+ticker.append(clone)
