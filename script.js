5a30a457450f671ddfd2b33b9036482b2c8cd8e3
Glitch (glitch-blank-website) <none>
2021-12-10 06:53:37 +0000

    ⭐️🏍 Checkpoint ./style.css:35586298/3499 ./index.html:35586298/4310

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const sectionTag = document.querySelector("section");
 const button = document.querySelector("button");
 
 button.addEventListener("click", function() {
-  sectionTag.classList.toggle("");
+  sectionTag.classList.toggle("new-class");
 });
