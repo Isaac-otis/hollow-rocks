2482822802a2040f11387c04aee2268d459ef26b
Glitch (glitch-blank-website) <none>
2021-12-10 05:34:14 +0000

    🍡🕵 Checkpoint ./style.css:35586298/22 ./index.html:35586298/41 ./script.js:35586298/792

--- script.js
+++ script.js
@@ -2,10 +2,6 @@
 const sectionTag = document.querySelector("section");
 const button = document.querySelector("button");
 
-
-// we'll add an event listener to the button to check for clicks
-button.addEventListener("click", function() {
-  
-  // we'll toggle a class name and then use that particular class to add our styles
-  sectionTag.classList.toggle("new-class");
+buttonTag.addEventListener("click", function() {
+  mainTag.classList.toggle("menu-open");
 });
