1425dc2fcdbc421011dc0d7cf25804c0de917073
Isaac-otis <95660286+Isaac-otis@users.noreply.github.com>
2021-12-13 15:17:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-// my function is to rotate the font colors of the poems
-
-
-// We'll create two constants
-const sectionTag = document.querySelector("section");
-const button = document.querySelector("button");
-
-button.onclick =12function(){"section"};
-button.addEventListener("click",function() {
-
-  sectionTag.classList.toggle("new-class");
-
-})
-
-
-
-
-// we set up 3 constants
-// 1. the ticker wrapper (the area where the images are scrolling)
-// 2. the list of items
-// 3. the copy (clone of our list)
-const ticker = document.querySelector('.ticker')
-const list = document.querySelector('.ticker__list')
-const clone = list.cloneNode(true)
-
-// then we keep appending our list inside the ticker
-ticker.append(clone)
-
-
-
-//NICOLE DON'T WORRY I WILL HAVE THIS IN GITHUB!
-
+function toggleTheme() {
+            // Obtains an array of all <link>
+            // elements.
+            // Select your element using indexing.
+            var theme = document.getElementsByTagName('link')[0];
+  
+            // Change the value of href attribute 
+            // to change the css sheet.
+            if (theme.getAttribute('href') == 'style.css') {
+                theme.setAttribute('href', 'alternate.css');
+            } else {
+                theme.setAttribute('href', 'style.css');
+            }
+        }
